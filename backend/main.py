cb400777000cf642b6f59bfb17fe88ae580cf852
Alexandre LAM <alexandrelam@outlook.com>
2020-11-21 14:25:26 +0100

    exemple of search

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,11 @@ odoo = Odoo()
 
 odoo.connect()
 
+# exemple : on cherche l'evenement avec un id de 2
+#           et on affiche que la propriete name et
+#           l organisateur de l evenement
+print(odoo.searchRead("event.event", [
+      ["id", "=", 2]], ["name", "organizer_id"]))
+
+# exemple : sans filtre, on affiche tous les evenements
 print(odoo.searchRead("event.event", [], ["name"]))
