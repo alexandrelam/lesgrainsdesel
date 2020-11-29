a6713afbcf4e65fd63cfa296d0a7187ea2d11f9d
Alexandre LAM <alexandrelam@outlook.com>
2020-11-29 14:10:48 +0100

    dynamic detail view

--- django_test/events/urls.py
+++ django_test/events/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('<id>', views.home, name='home'),
 ]
--- django_test/events/views.py
+++ django_test/events/views.py
@@ -3,7 +3,8 @@ from .models import Event
 # Create your views here.
 
 
-def home(request):
+def home(request, id):
     sorted_events_list = Event.objects.order_by('date_begin')
     context = {'eventsList': sorted_events_list}
+    context["selected"] = Event.objects.get(id=id)
     return render(request, 'events/details.html', context)
