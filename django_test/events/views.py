66f8a5bac2ad1c33d9967f83bb4f7b6525234bb7
Alexandre LAM <alexandrelam@outlook.com>
2020-11-29 12:53:42 +0100

    template ajout des events depuis la base de données

--- django_test/events/views.py
+++ django_test/events/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'events/details.html')
+    sorted_events_list = Event.objects.order_by('date_begin')
+    context = {'eventsList': sorted_events_list}
+    return render(request, 'events/details.html', context)
