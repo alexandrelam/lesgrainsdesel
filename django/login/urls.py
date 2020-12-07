8740e75305736a136d2f8acf868459581a903307
alexandrelam <alexandrelam@outlook.com>
2020-12-07 09:13:45 +0100

    login page form working with django auth

--- django/login/urls.py
+++ django/login/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login),
+    path('', views.login_view),
 ]
 
--- django/login/views.py
+++ django/login/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import redirect
+from events import views as events_views
 
-# Create your views here.
-def login(request):
+@csrf_protect
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect(events_views.redirect_view)
     return render(request, 'login/login.html')
