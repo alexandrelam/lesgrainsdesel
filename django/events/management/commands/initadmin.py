3a05fdaf7ce3abe37f7f941fe4513a9b41344dc1
Alexandre LAM <alexandrelam@outlook.com>
2020-12-18 13:40:04 +0100

    automatic migration and admin creation

---
+++ django/events/management/commands/initadmin.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if User.objects.count() == 0:
+            username = 'admin'
+            email = 'admin@admin.com'
+            password = 'admin'
+            print('Creating account for %s (%s)' % (username, email))
+            admin = User.objects.create_superuser(
+                email=email, username=username, password=password)
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+        else:
+            print('Admin accounts can only be initialized if no Accounts exist')
