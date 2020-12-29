d697dd1699ec1f75bd04381c030dc926bff7abc5
alexandrelam <alexandrelam@outlook.com>
2020-12-29 21:22:55 +0100

    ajout des pages restantes à compléter

--- django/events/urls.py
+++ django/events/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.redirect_view),
     path('events/<id>', views.home, name='home'),
+    path('create/', views.create_events, name="create_events"),
+    path('participations/', views.participations, name="participations"),
 ]
