4ce83b69fb2cdd90f76f4645f9b89f68c2734196
alexandrelam <alexandrelam@outlook.com>
2020-12-31 12:12:14 +0100

    added link to "Les événements" nav

--- django/events/urls.py
+++ django/events/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.redirect_view),
-    path('events/<id>', views.home, name='home'),
+    path('', views.redirect_view, name='home'),
+    path('events/<id>', views.home),
     path('create/', views.create_events, name="create_events"),
     path('participations/', views.participations, name="participations"),
 ]
