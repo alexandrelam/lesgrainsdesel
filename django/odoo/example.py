40e9a6200d34798c611a5802b44f436ac7ccff77
= <pizzaman@localhost.localdomain>
2021-01-17 22:36:17 +0100

    [FIX] Fixed the connection between django and odoo

--- django/odoo/example.py
+++ django/odoo/example.py
@@ -13,8 +13,9 @@ odoo.connect()
 # exemple : sans filtre, on affiche tous les evenements
 # verifier bien que l'id de la personne existe sinon
 # changez l id
-# print(odoo.searchRead("res.partner", [['id', '=', 7]],
-#                    ["name", "age", "contact_address", "phone"]))
 
-odoo.createEvent("event_test5", "2020-12-28 20:18:18",
-                 "2020-12-29 20:18:18", 6)
+print(odoo.searchRead("res.partner", [['id', '=', 1]],
+                    ["name", "age", "contact_address", "phone"]))
+
+#odoo.createEvent("event_test5", "2020-12-28 20:18:18",
+#                 "2020-12-29 20:18:18", 6)
